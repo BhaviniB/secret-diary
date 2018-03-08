884bd950b620d7c42a64db9258ae0445fbf0f6f1
BhaviniB <lassiebhavini@gmail.com>
2018-03-08 23:58:24 +0530

    secret diary

---
+++ sd.php
@@ -0,0 +1,7 @@
+<form method="post">
+
+<input type="email" name="email" placeholder="Your Email">
+<input type="password" name="password" placeholder="Password">
+<input type="checkbox" name="stayLoggedIn" value=1>
+<input type="submit" value="Sign Up!"> 
+</input>
