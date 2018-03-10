dbac61e5d52e755030b066de2f2536ee4d17b357
BhaviniB <lassiebhavini@gmail.com>
2018-03-11 00:00:51 +0530

    database connection and form

--- sd.php
+++ sd.php
@@ -1,7 +1,38 @@
+<?php
+if(array_key_exists("submit",$_POST))
+{
+	
+	error="";
+	
+	if(!_POST['email'])
+	{
+		$error.="An email address is required<br>";
+	}
+ 	if(!_POST['password'])
+	{
+		$error.="A password address is required<br>";
+	}
+ 	
+	if ($error!="")
+	{
+	$error="<p>There were errors in your form:</p>".$error;	
+		
+	}
+}
+
+
+
+?>
+<div id="error">
+<?php
+echo $error;
+?>
+</div>
 <form method="post">
 
 <input type="email" name="email" placeholder="Your Email">
 <input type="password" name="password" placeholder="Password">
 <input type="checkbox" name="stayLoggedIn" value=1>
-<input type="submit" value="Sign Up!"> 
+<input type="submit" name="submit" value="Sign Up!"> 
 </input>
+</form>
