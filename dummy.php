27adc071f513f415dbc3ada47d36b8239fca9667
BhaviniB <lassiebhavini@gmail.com>
2018-03-16 22:31:49 +0530

    bootstrap added

--- dummy.php
+++ dummy.php
@@ -107,26 +107,63 @@
   <div id="error"><?php echo $error; ?></div>
 
 
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-<!-- SIGNUP FORM -->
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <div class="container">
+   <h1>SECRET DIARY</h1>
+    <!-- SIGNUP FORM -->
   <form method = "post">
   	
-  	<input type="email" name="email" placeholder="Your Email">
-  	<input type="password" name="password" placeholder="Your password">
-  	<input type="checkbox" name = "stayLoggedIn" value="1">
-  	<input type="hidden" name="signUp" value="1">
-  	<input type="submit" name="submit" value ="SignUp">
-
+  <fieldset class="form-group">
+  <input class="form-control" type="email" name="email" placeholder="Your Email">
+  </fieldset>
+<fieldset class="form-group">
+  	<input class="form-control" type="password" name="password" placeholder="Your password">
+	</fieldset>
+  <fieldset class="form-group">	
+  <input class="form-control" type="checkbox" name = "stayLoggedIn" value="1">
+ 
+  	<input class="form-control" type="hidden" name="signUp" value="1">
+  	</fieldset>
+	<fieldset class="form-group">
+	<input class="form-control" type="submit" name="submit" value ="SignUp">
+</fieldset>
   </form>
 <!-- LOGIN FORM -->
   <form method = "post">
-  	
-  	<input type="email" name="email" placeholder="Your Email">
-  	<input type="password" name="password" placeholder="Your password">
-  	<input type="checkbox" name = "stayLoggedIn" value="1">
-  	<input type="hidden" name="signUp" value="0">
-
-  	<input type="submit" name="submit" value ="Log In">
-
+<fieldset class="form-group">  	
+  	<input class="form-control" type="email" name="email" placeholder="Your Email">
+  	</fieldset>
+	<fieldset class="form-group">
+	<input class="form-control" type="password" name="password" placeholder="Your password">
+  	 	</fieldset>
+	<fieldset class="form-group">
+	<input class="form-control" type="checkbox" name = "stayLoggedIn" value="1">
+  	 	</fieldset>
+	<fieldset class="form-group">
+	<input class="form-control" type="hidden" name="signUp" value="0">
+ 	
+  	<input class="form-control" type="submit" name="submit" value ="Log In">
+ 	</fieldset>
   </form>
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
+
